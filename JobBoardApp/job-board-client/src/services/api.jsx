32bb7ce36f10907c5da4293f07bec4a96cf5bf10
Nathan Chan <nc8334@gmail.com>
2026-03-31 15:10:08 -0400

    fix: localhost listening port

--- JobBoardApp/job-board-client/src/services/api.jsx
+++ JobBoardApp/job-board-client/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'http://localhost:5142/api',
 });
 
 // Automatically attach JWT token to every request
